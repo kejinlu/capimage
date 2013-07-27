59cfd259b786910aa33f5ce4d2f93ac7b5b58466
Luke <kejinlu@gmail.com>
2013-07-27 16:16:11 +0800

    install之间修改名字

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 from distutils.core import setup
+import os
+import shutil
+
+if not os.path.exists('scripts'):
+    os.makedirs('scripts')
+shutil.copyfile('capimage.py', 'scripts/capimage')
 
 setup(
     name = 'capimage',
     version = '0.1',
-    install_requires = ['sqlalchemy'],
     scripts=['capimage.py']
     )
